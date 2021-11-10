ccb7e50c0e910d8369ec61c5170d46795bcbc79e
Ryan Walmsley <ryan@smartlm.co.uk>
2021-11-10 12:43:55 +0000

    Lol

--- Source/content_script.js
+++ Source/content_script.js
@@ -39,10 +39,8 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bLinux\b/g, "GNU Plus Linux");
+	v = v.replace(/\blinux\b/g, "GNU Plus Linux");
 	
 	textNode.nodeValue = v;
 }
